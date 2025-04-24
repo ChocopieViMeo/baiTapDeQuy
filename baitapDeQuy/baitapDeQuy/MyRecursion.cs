cb57da01947ecc36c4b298a63ba33c9df7963360
Trần Minh Tuyên <trminhtuyen1905@gmail.com>
2025-04-24 17:27:31 +0700

    Cleaned project files. Removed build artifacts.

---
+++ baitapDeQuy/baitapDeQuy/MyRecursion.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baitapDeQuy
+{
+    internal class MyRecursion
+    {
+        // tính giai thừa
+        public int Factorial ( int n)
+        {
+            if (n == 0) return 1;
+            else return n * Factorial(n - 1);
+        }
+        // tính lũy thừa
+        public double LuyThua ( double a, double b )
+        {
+            if (b == 0) return 1;
+            else return a * LuyThua(a, b - 1);
+        }
+        // fibonacci
+        public int Fibonacci ( int n)
+        {
+            if (n == 1 || n == 2) return 1;
+            else return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+        // đảo ngược chuỗi
+        public string Reverse( int n)
+        {
+            if (n == 0) return " ";
+            else
+            {
+                Console.Write(n % 10);
+                return Reverse(n / 10);
+            }
+        }
+        // tính tổng các chữ số của 1 số nguyên
+        public int SumDigits ( int n )
+        {
+            if (n == 0) return 0;
+            return (n % 10) + SumDigits(n / 10);
+        }
+        // in dãy số từ 1 đến n bằng đệ quy
+        public void Print1ToN( int n)
+        {
+            if (n == 0) return;
+            Print1ToN(n - 1);
+            Console.Write(n + " ");
+        }
+    }
+}
---
+++ baitapDeQuy/baitapDeQuy/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baitapDeQuy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyRecursion rec = new MyRecursion();
+            int choice = -1;
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+            do
+            {
+                Console.WriteLine("=============================");
+                Console.WriteLine("1. Tính Giai Thừa ");
+                Console.WriteLine("2.Tính Lũy Thừa ");
+                Console.WriteLine("3. Tính Fibonacci ");
+                Console.WriteLine("4. Đảo ngược chuỗi ");
+                Console.WriteLine("5. Tính tổng chữ số của 1 số nguyên ");
+                Console.WriteLine("6. In dãy từ 1 đến N"); ;
+                Console.WriteLine("==============================");
+                choice = int.Parse(Console.ReadLine());
+                switch ( choice)
+                {
+                    case 1:
+                        {
+                            Console.Write(" Nhập n: ");
+                            int n = int.Parse(Console.ReadLine());
+                            Console.WriteLine($"Giai thừa của {n} là : {rec.Factorial(n)}"); 
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.Write("Nhập số a: ");
+                            int a = int.Parse(Console.ReadLine());
+                            Console.Write("Nhập số b: ");
+                            int b = int.Parse(Console.ReadLine());
+                            Console.WriteLine($"{a} ^ {b} = {rec.LuyThua(a, b)}");
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.Write("Nhập số n: ");
+                            int n = int.Parse(Console.ReadLine());
+                            Console.WriteLine($"Fibonacci {n} = {rec.Fibonacci(n)}");
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.Write("Nhập n: ");
+                            int n = int.Parse(Console.ReadLine());
+                            Console.WriteLine($"Sau khi đảo chuỗi : {rec.Reverse(n)}");
+                            break;
+                        }
+                    case 5:
+                        {
+                            Console.Write("Nhập số nguyên n: ");
+                            int n = int.Parse(Console.ReadLine());
+                            Console.WriteLine($"Tổng {n} : {rec.SumDigits(n)}");
+                            break;
+                        }
+                    case 6:
+                        {
+                            Console.Write("Nhập số n: ");
+                            int n = int.Parse(Console.ReadLine());
+                            Console.WriteLine($"In từ 1 đến {n} : ");
+                            rec.Print1ToN(n);
+                            break;
+                        }
+                }
+
+            } while (choice != 0);
+
+        }
+    }
+}
